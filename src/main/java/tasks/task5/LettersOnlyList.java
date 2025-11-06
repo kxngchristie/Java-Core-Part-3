ac43fbae5b7dd98274dc13d63d6371248637be0c
Kemar Christie <kchristie@griddynamics.com>
2025-11-06 13:21:17 -0500

    Refactor LettersOnlyList: Update comments for clarity on map usage and sorting behavior

--- src/main/java/tasks/task5/LettersOnlyList.java
+++ src/main/java/tasks/task5/LettersOnlyList.java
@@ -11,7 +11,8 @@ public class LettersOnlyList {
     static void main(String[] args) {
 
         // Task 14: Letters-Only List Implementation
-        Map<String, List<String>> people = new HashMap<>();
+        Map<String, List<String>> people = new HashMap<>(); // Random Insertion Order
+        // Map<String, List<String>> people = new LinkedHashMap<>(); // Preserved Insertion Order
         people.put("John", Arrays.asList("555-1123", "s", "555-3389", "a"));
         people.put("Mary", Arrays.asList("555-2243", "z", "555-5264"));
         people.put("Steve", Arrays.asList("555-6654", "555-3242", "d"));
@@ -19,7 +20,7 @@ public class LettersOnlyList {
         List<String> lettersOnlyList = people.values().stream()
                 .flatMap(List::stream)
                 .filter(str -> str.matches("[a-zA-Z]"))
-                .sorted(Comparator.naturalOrder())
+                .sorted(Comparator.naturalOrder()) // Can be Removed if the LinkedList Implementation is used tho it wont be natural order it will be the original insertion order
                 .toList();
 
         printFancyTable(lettersOnlyList, "Task 14: Letters Only from Map Values");
