ef91043c0a2a5857c2c46ac38ae5f35bd070e070
Kemar Christie <kchristie@griddynamics.com>
2025-11-06 13:24:20 -0500

    Update LettersOnlyListTest: Add comment to clarify the use of LinkedHashMap for preserved insertion order

--- src/test/java/tasks/task5test/LettersOnlyListTest.java
+++ src/test/java/tasks/task5test/LettersOnlyListTest.java
@@ -16,6 +16,7 @@ public class LettersOnlyListTest {
     @Test
     void testLettersOnlyList() {
         Map<String, List<String>> people = new HashMap<>();
+        // Map<String, List<String>> people = new LinkedHashMap<>(); // Preserved Insertion Order
         people.put("John", Arrays.asList("555-1123", "s", "555-3389", "a"));
         people.put("Mary", Arrays.asList("555-2243", "z", "555-5264"));
         people.put("Steve", Arrays.asList("555-6654", "555-3242", "d"));
